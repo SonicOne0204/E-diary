a02b60d11d08505408fb38ab6d208ab462e14172
SonicOne0204 <bgejts38@gmail.com>
2025-07-09 20:03:40 +0600

    Added get and delete endpoints, added status options

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ from app.api.v1.endpoints.subjects import subject_router
 from app.api.v1.endpoints.homeworks import homeworks_router
 from app.api.v1.endpoints.schedules import schedules_router
 from app.api.v1.endpoints.teacher import teacher_router
+from app.api.v1.endpoints.attendance import attendances_router
 from app.logging.logger import *
 
 app = FastAPI()
@@ -22,3 +23,4 @@ app.include_router(subject_router)
 app.include_router(homeworks_router)
 app.include_router(schedules_router)
 app.include_router(teacher_router)
+app.include_router(attendances_router)
